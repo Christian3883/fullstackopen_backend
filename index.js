ebc05afb5cca29b058ae7866b492bf5930e63717
Christian Gomez <goni_83@msn.com>
2021-06-18 08:53:15 -0300

    Feat: Add Cors

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
 app.use(morgan('combined'))
-/*
-app.use(morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms'
-    ].join(' ')
-  }))
-*/  
+app.use(cors())
 
 let persons = [
     {
